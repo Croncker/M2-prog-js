0ce72847a08b4cdb69b8fbc2fa6bca20f7ae275b
Lucca <35749@ma-web.nl>
2022-11-22 12:26:21 +0100

    Les1

---
+++ les1/app.js
@@ -0,0 +1,24 @@
+class App
+{
+    runApplication()
+    {
+        console.log("Hello World");
+        let appnaam = "Facebook"
+        let versienummer = ("0.9 double")
+        let versiedatum = ("22/11/2022")
+        let autheur = ("Lucca")
+        let copyright = ("Gebruik deze code niet")
+        let distributeur = ("Lucca")
+        let darkmode = (true)
+        console.log(appnaam)
+        console.log(versienummer)
+        console.log(versiedatum)
+        console.log(autheur)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkmode)
+    }
+}
+
+let app = new App();
+app.runApplication();
